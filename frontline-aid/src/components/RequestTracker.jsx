71d25d183484c028edfd36e4cb38e7062d40083b
Shreyans Pathak <pathak.shreyans@gmail.com>
2021-11-07 05:30:32 -0500

    fix: Changes till now

---
+++ frontline-aid/src/components/RequestTracker.jsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+  { field: 'id', headerName: 'S.No', width: 90 },
+  {
+    field: 'firstName',
+    headerName: 'Name',
+    width: 150,
+    editable: false,
+  },
+  {
+    field: 'type',
+    headerName: 'Request Type',
+    width: 150,
+    editable: false,
+  },
+  {
+    field: 'status',
+    headerName: 'Request Status',
+    width: 110,
+    editable: true,
+  },
+  
+];
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+
+export default function DataGridDemo() {
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+      />
+    </div>
+  );
+}
